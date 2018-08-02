5c1d2033b2c432e662b229d6ff5a238983ac4063
megaloden106 <eddielo106@gmail.com>
2018-08-01 21:15:28 -0700

    Complete pseudoclassical stack/queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
+  this.nextInQueue = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.index - this.nextInQueue > 0) {
+    this.nextInQueue++;
+    return this.storage[this.nextInQueue - 1];
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.index - this.nextInQueue;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
 };
 
+Stack.prototype.size = function() {
+  return this.index;
+};
 
+Stack.prototype.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.index > 0) {
+    this.index--;
+    var last = this.storage[this.index];
+    delete this.storage[this.index];
+    return last;
+  }
+};
